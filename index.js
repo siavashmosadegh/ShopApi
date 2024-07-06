53c8d7b11c07b8fc5b9e0b93de86fd4122c39eb8
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-06 20:23:57 +0330

    adding enviroment variables

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 app.get('/', (req, res) => {
     res.send('hello API !')
 })
 
 app.listen(3000, () => {
+    console.log(api);
     console.log("server is started now on http://localhost:3000");
 })
