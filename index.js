90ba7852a7809420df5453141c94f8c25ad741f0
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-06 20:42:42 +0330

    create first api call & parsing json data

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//middleware
+app.use(bodyParser.json());
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-    res.send('hello API !')
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product)
+})
+
+app.post (`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3000, () => {
