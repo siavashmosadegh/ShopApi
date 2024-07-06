77eee1191a3f76e5fd3ef7b5db0812c1fdf07681
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-06 20:48:17 +0330

    adding morgan ( The Library used to log api requests )

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 require('dotenv/config');
 
