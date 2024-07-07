d6cd799fd7f5228f33fe0a877e65db1eba4fda69
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-07 19:10:52 +0330

    connected the api to the MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongodb = require('mongodb')
 
 //middleware
 app.use(bodyParser.json());
@@ -25,6 +26,34 @@ app.post (`${api}/products`, (req, res) => {
     console.log(newProduct);
     res.send(newProduct);
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://siavash:QwerAsdfZxcv7676@shopapi.mgh6qsd.mongodb.net/?appName=shopAPI";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(3000, () => {
     console.log(api);
